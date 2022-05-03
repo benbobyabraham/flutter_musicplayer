714d484c7e6cdf480eed74a36a35d7a6bffa46a8
benbobyabraham <benbobyabraham@gmail.com>
2022-05-03 21:13:10 +0530

    3-5-2022.2:final

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,6 +29,59 @@ class MusicApp extends StatefulWidget {
 }
 
 class _MusicAppState extends State<MusicApp> {
+  bool playing = false;
+  IconData playBtn = Icons.play_arrow;
+
+  late AudioPlayer _player;
+  late AudioCache cache;
+
+  Duration position = new Duration();
+  Duration musicLength = new Duration();
+
+  Widget slider() {
+    return Container(
+      width: 300.0,
+      child: Slider.adaptive(
+          activeColor: Colors.blue.shade800,
+          inactiveColor: Colors.grey.shade700,
+          value: position.inSeconds.toDouble(),
+          max: musicLength.inSeconds.toDouble(),
+          onChanged: (value) {
+            seekToSec(value.toInt());
+          }),
+    );
+  }
+
+  void seekToSec(int sec) {
+    Duration newPos = Duration(seconds: sec);
+    _player.seek(newPos);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+    cache = AudioCache(fixedPlayer: _player);
+
+    // _player.durationHandler = (d) {
+    //   setState(() {
+    //     musicLength = d;
+    //   });
+    // };
+
+    // _player.positionHandler = (p) {
+    //   setState(() {
+    //     position = p;
+    //   });
+    // };
+
+    _player.onDurationChanged.listen((d) => setState(() => musicLength = d));
+
+    _player.onAudioPositionChanged.listen((p) => setState(() => position = p));
+
+    cache.load("middle_of_the_night.mp3");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +112,7 @@ class _MusicAppState extends State<MusicApp> {
                   child: Text(
                     "Walkman",
                     style: TextStyle(
-                      color: Colors.white,
+                      color: Colors.blue.shade900,
                       fontSize: 28.0,
                       fontWeight: FontWeight.bold,
                     ),
@@ -71,7 +125,7 @@ class _MusicAppState extends State<MusicApp> {
                   child: Text(
                     "Listen to your favorite Music",
                     style: TextStyle(
-                      color: Colors.white,
+                      color: Colors.blue.shade900,
                       fontSize: 22.0,
                       fontWeight: FontWeight.w100,
                     ),
@@ -119,14 +173,66 @@ class _MusicAppState extends State<MusicApp> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
+                            Container(
+                              width: 500.0,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "${position.inMinutes}: ${position.inSeconds.remainder(60)}",
+                                    style: TextStyle(
+                                      fontSize: 10.0,
+                                    ),
+                                  ),
+                                  slider(),
+                                  Text(
+                                    "${musicLength.inMinutes}: ${musicLength.inSeconds.remainder(60)}",
+                                    style: TextStyle(
+                                      fontSize: 10.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                             Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 IconButton(
+                                  iconSize: 45.0,
+                                  color: Colors.teal.shade800,
                                   onPressed: () {},
                                   icon: Icon(
                                     Icons.skip_previous,
                                   ),
                                 ),
+                                IconButton(
+                                  iconSize: 62.0,
+                                  color: Colors.teal.shade800,
+                                  onPressed: () {
+                                    if (!playing) {
+                                      cache.play("middle_of_the_night.mp3");
+                                      setState(() {
+                                        playBtn = Icons.pause;
+                                        playing = true;
+                                      });
+                                    } else {
+                                      _player.pause();
+                                      setState(() {
+                                        playBtn = Icons.play_arrow;
+                                        playing = false;
+                                      });
+                                    }
+                                  },
+                                  icon: Icon(playBtn),
+                                ),
+                                IconButton(
+                                  iconSize: 45.0,
+                                  color: Colors.teal.shade800,
+                                  onPressed: () {},
+                                  icon: Icon(Icons.skip_next),
+                                )
                               ],
                             )
                           ],
